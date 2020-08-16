bc03cea5fa529b4293a33bdf65b5dd6030e5da47
Zhuhaofan <494019287@qq.com>
2020-08-16 22:05:29 +0800

    master

---
+++ html-music/js/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  const myAudio = document.getElementById("music")
+  const myPlay = document.getElementById("play")
+  const myAccelerate = document.getElementById("accelerate")
+  const myShowDown = document.getElementById("showDown")
+
+// 可以用nodeJs 获取content/music里面的文件然后导入这个数组，这里先使用自制数组来使用
+  let MusicArry = ['mom.mp3',];
+
+// 查看是否获取到了元素
+  console.log(myPlay);
+  console.log(myAudio);
+
+// // 为music 添加音乐
+//   myAudio.autoplay = true
+//   myAudio.src
+
+// 
+}
